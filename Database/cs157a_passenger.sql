01665a6ac7bdc05dbd3c60e284b235a849cadca4
Arjay123 <arjaynguyen123@hotmail.com>
2013-10-28 20:37:44 -0700

    Added triggers to database
    Added 2 triggers to database
    -change seat.taken to true when a passenger is added
    -change seat.take to false when a passenger is deleted

--- Database/cs157a_passenger.sql
+++ Database/cs157a_passenger.sql
@@ -30,7 +30,8 @@ CREATE TABLE `passenger` (
   `flightID` int(11) DEFAULT NULL,
   `firstClass` tinyint(1) DEFAULT NULL,
   `seatID` char(3) DEFAULT NULL,
-  `pasID` char(4) DEFAULT NULL
+  `pasID` char(4) NOT NULL,
+  PRIMARY KEY (`pasID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,6 +44,50 @@ LOCK TABLES `passenger` WRITE;
 INSERT INTO `passenger` VALUES ('Arjay Nguyen',22,101,1,'A08','101C'),('Jim Nelson',54,101,0,'A01','102C'),('Billy Norton',19,101,0,'A02','103C'),('Allison Bingham',32,101,0,'A03','104C'),('Lucy Chen',25,101,0,'A04','105C'),('Shivalik Narad',22,101,1,'A07','106C'),('Andrew Jones',25,101,0,'A05','107C'),('Tyler Stennette',23,101,0,'A06','108C'),('Joseph Coorey',23,101,0,'B01','109C'),('Joshua Filstrup',24,101,0,'B02','110C');
 /*!40000 ALTER TABLE `passenger` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER updateSeat
+AFTER INSERT ON Passenger
+FOR EACH ROW
+BEGIN 
+UPDATE Seat SET Seat.taken = true
+WHERE Seat.sID = new.seatID;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER updateSeatFalse
+BEFORE DELETE ON Passenger
+FOR EACH ROW
+BEGIN 
+UPDATE Seat SET Seat.taken = false
+WHERE Seat.sID = old.seatID;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-10-28 19:36:54
+-- Dump completed on 2013-10-28 20:36:12
